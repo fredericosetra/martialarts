39289ccd2366b6d6d2d149577da0250cf9066854
Frederico Setra <frederico_setra@hotmail.com>
2022-01-22 14:20:17 -0300

    feat: Create News pages

---
+++ martialarts/src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <h1>Sou uma tela de </h1>
+      <h1>Login</h1>
+    </>
+  );
+};
+
+export default Login;
---
+++ martialarts/src/pages/studentRegistration/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StudentRegistration = () => {
+  return (
+    <>
+      <h1>sou um tela de cadastro de alunos</h1>
+      <h1>me use</h1>
+    </>
+  );
+};
+
+export default StudentRegistration;
