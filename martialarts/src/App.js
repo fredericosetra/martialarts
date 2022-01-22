bac414bf0e748fe54f8e62928ac00a653f3b1b2f
Frederico Setra <frederico_setra@hotmail.com>
2022-01-22 14:17:46 -0300

    feat: first commit

--- martialarts/src/App.js
+++ martialarts/src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
-import Home from "./home/home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import StudentRegistration from "./pages/studentRegistration";
+import Sidebar from "./components/Sidebar";
+import TopBar from "./components/Topbar";
+import * as S from "./AppStyled";
 
 function App() {
   return (
     <>
-      <div>Hello World</div>
-      <Home />
+      <S.Container>
+        <Router>
+          <Sidebar />
+          <S.AlignContent>
+            <TopBar />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/studentregistration"
+                element={<StudentRegistration />}
+              />
+            </Routes>
+          </S.AlignContent>
+        </Router>
+      </S.Container>
     </>
   );
 }
---
+++ martialarts/src/AppStyled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const AlignContent = styled.div`
+  flex-direction: "collumn";
+  width: 100%;
+`;
